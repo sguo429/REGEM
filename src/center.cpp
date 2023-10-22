d82c5005cdcb5dbe1635d1a84e74ce13093e350d
sguo429 <148508089+sguo429@users.noreply.github.com>
2023-10-22 18:34:14 -0500

    Update center.cpp

--- src/center.cpp
+++ src/center.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <cmath>
 
-void transformBetaAndCovariance(
+void centerConversion(
     std::vector<double>& beta, 
     std::vector<double>& mb_v, 
     const std::unordered_map<std::string, double>& meanValues, 
@@ -25,51 +25,33 @@ void transformBetaAndCovariance(
     // Create matrix C
     std::vector<std::vector<double>> C(dim, std::vector<double>(dim, 0.0));
     C[0][0] = 1.0;
-    for (size_t i = 1; i < dim; ++i) {
+    for (size_t i = 1; i < dim; i++) {
         C[i][i] = 1.0;
         C[0][i] = orderedMeanValues[i - 1];
     }
 
     // Create matrix M
-    size_t mDim = 1 + 2*(dim-1) + (dim-1)*(dim-2)/2;
+    size_t mDim = dim*dim;
     std::vector<std::vector<double>> M(mDim, std::vector<double>(mDim, 0.0));
 
-    // Set all diagonal values of M to 1
-    for (size_t i = 0; i < mDim; ++i) {
+    for (size_t i = 0; i < mDim; i++) {
         M[i][i] = 1.0;
     }
 
-    // First row of M
-    size_t offset = 2 * dim - 1;
-    for (size_t i = 1; i < dim; ++i) {
+    for (size_t i = 1; i < dim; i++) {
         M[0][i] = 2 * orderedMeanValues[i - 1];
-        M[0][dim + i - 1] = std::pow(orderedMeanValues[i - 1], 2);
-        for (size_t j = i + 1; j < dim; ++j) {
-            M[0][offset] = 2 * orderedMeanValues[i - 1] * orderedMeanValues[j - 1];
-            ++offset;
-        }
-    }
-
-    // Next p rows of M
-    size_t offset = 2 * dim - 1;
-    for (size_t i = 1; i < dim; ++i) {
-        for (size_t j = 1; j < dim; ++j) {
-            if (i == j) {
-                M[i][dim + j - 1] = orderedMeanValues[j - 1];
-            } else{
-              M[i][offset] = orderedMeanValues[j + i - 1];
-              ++offset;
-            } 
-            if(i>1){
-                M[i][2 * dim + i - 3]= orderedMeanValues[0];
-            }
+        M[0][(i * dim) + i] = std::pow(orderedMeanValues[i - 1], 2);
+        M[i][(i * dim) + i] = orderedMeanValues[i - 1];
+        for (size_t j = i + 1; j < dim; j++) {
+            M[0][(i * dim) + j] = 2 * orderedMeanValues[i - 1] * orderedMeanValues[j - 1];
+            M[i][(i * dim) + j] = orderedMeanValues[j - 1];
         }
     }
 
     // Multiply beta with C
     std::vector<double> newBeta(dim, 0.0);
-    for (size_t i = 0; i < dim; ++i) {
-        for (size_t j = 0; j < dim; ++j) {
+    for (size_t i = 0; i < dim; i++) {
+        for (size_t j = 0; j < dim; j++) {
             newBeta[i] += C[i][j] * beta[j];
         }
     }
@@ -77,8 +59,8 @@ void transformBetaAndCovariance(
 
     // Multiply mb_v with M
     std::vector<double> newMb_v(mDim, 0.0);
-    for (size_t i = 0; i < mDim; ++i) {
-        for (size_t j = 0; j < mDim; ++j) {
+    for (size_t i = 0; i < mDim; i++) {
+        for (size_t j = 0; j < mDim; j++) {
             newMb_v[i] += M[i][j] * mb_v[j];
         }
     }
