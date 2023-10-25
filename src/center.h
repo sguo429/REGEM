970c8234be7a05f17a2f958ccf68a7563c639910
sguo429 <148508089+sguo429@users.noreply.github.com>
2023-10-24 20:57:28 -0500

    Update center.h

--- src/center.h
+++ src/center.h
@@ -12,8 +12,6 @@ void centerConversion(
     std::unordered_map<std::string, double> meanValues, 
     std::vector<std::string> interaction_names, 
     size_t dim,
-    std::vector<int> betaIntIndex, 
-    std::vector<int> mbCovIndex,
     int nExp,
     int centerIn,
     int centerOut);
@@ -24,8 +22,6 @@ void centerConversion_rb(
     std::unordered_map<std::string, double> meanValues, 
     std::vector<std::string> interaction_names, 
     size_t dim,
-    std::vector<int> betaIntIndex, 
-    std::vector<int> rbCovIndex,
     int nExp,
     int centerIn,
     int centerOut);
