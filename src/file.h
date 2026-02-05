1132dd054a772c0b56c28b27131246cbc196377a
Shuyi Guo <148508089+sguo429@users.noreply.github.com>
2026-02-05 17:00:19 -0600

    Update file.h

--- src/file.h
+++ src/file.h
@@ -6,7 +6,7 @@ class FileInfo {
         std::vector<std::string> stratum_names;
 
         int dim;
-        double sigma2;
+        double sigma2 = -1.0;
 
         bool robust = false;
         size_t nVarInfo = 0;
@@ -37,5 +37,6 @@ void get_variantInfo_indices(std::unordered_map<std::string, int> columnNames, F
 void get_marginal_indices(std::unordered_map<std::string, int> columnNames, FileInfo* fip);
 
 void generate_subcategorical_columns(size_t nInt1, std::unordered_map<std::string, int> columnNames, std::vector<std::string> interactions, std::vector<std::string> categorical_columns, FileInfo* fip);
+
 
 #endif
